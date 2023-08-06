4fa57544f6a40192df12363ccaadaffeb567ec84
Luizfernando5212 <luiz.5.2.1.luiz@gmail.com>
2023-08-06 17:57:54 -0300

    Adding infos on README file

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # whatsappApiHelper
-This is a repositorry with the most used JSONs for interacting  with Whatsap business Api
+
+This is a repository with some of the most used Objects for interacting with Whatsap Business Api, I created this since I am currently working in a privete project that require this "module". And while looking for something like this on the internet I only found some stuff that required payment.
+
+This structure should be easy to replicate in any other langagues, or using any other technologies.
+
+## Technologies
+
+- Node.js
+- dotenv
+- Express.js
+- Axios
+
+## How to run
+
+```
+// Clone repository
+git clone https://github.com/Luizfernando5212/whatsappApiHelper.git
+
+// Install dependencies
+npm install dotenv express axios
+
+// Be creative
+
+// To be able to run and test locally, you should install ngrok to redirect. The Facebook App requires that the URL is not Local.
+npm install ngrok
+// more info on https://www.npmjs.com/package/ngrok
+```
+
+## Examples
+
+I made these, so that it should be simple to use and replicate, to make a request of a message as simple as this
+
+```
+await axios(whatsHelper.textMessage(from, "Hello World!"));
+await axios(whatsHelper.interactiveMessage(from, 'My message', ['Button1', 'Button2'], 1));
+```
+
+There are other examples in the example.js file
+
+
+## Author
+
+Luiz Fernando Arruda Oliveira
+
+https://www.linkedin.com/in/luiz-oliveira-037470213/
--- helper.js
+++ helper.js
@@ -34,15 +34,15 @@ exports.textMessage = (from, message) => {
 }
 
 /* This is to send the text with header, body, footer and buttons.
-   message is an object with header, body and footer.
-   {
-        header: "message inside header",
-        body: "message inside body",
-        footer: "message inside footer"
-   }
-   buttons is an array of strings, limited to 3 buttons, according to limitations of this kind o interaction.
-
-   The i parameter is to keep track of the state of the users interactions.
+ * message is an object with header, body and footer.
+ * {
+ *      header: "message inside header",
+ *      body: "message inside body",
+ *      footer: "message inside footer"
+ * }
+ * buttons is an array of strings, limited to 3 buttons, according to limitations of this kind o interaction.
+ *
+ * The i parameter is to keep track of the state of the users interactions.
 */
 exports.fullMessage = (from, message, buttons, i = 0) => {
     let body = {
@@ -88,14 +88,14 @@ exports.fullMessage = (from, message, buttons, i = 0) => {
 }
 
 /*
-    This is to send the text with header, body and buttons. Usualy footer is not used, so it is not included in this function.
-    message is an object with header and body or just a message that have buttons, in that case this parameter receives a String.
-    {
-        header: "message inside header",
-        body: "message inside body"
-    }
-
-    The i parameter is to keep track of the state of the users interactions.
+ *  This is to send the text with header, body and buttons. Usualy footer is not used, so it is not included in this function.
+ *  message is an object with header and body or just a message that have buttons, in that case this parameter receives a String.
+ *  {
+ *      header: "message inside header",
+ *      body: "message inside body"
+ *  }
+ *
+ *  The i parameter is to keep track of the state of the users interactions.
 */
 exports.interactiveMessage = (from, message, buttons, i = 0) => {
     if (message instanceof Object) {
@@ -172,11 +172,11 @@ exports.interactiveMessage = (from, message, buttons, i = 0) => {
 }
 
 /*
-    This is to send an interactive list message, the buttons show up like a dropdown.
-    message is a String with the message that will be shown.
-    buttons is an array of strings, limited to 20 buttons, according to limitations of this kind o interaction.
-    name is a String that will be shown in the dropdown button.
-    i is to keep track of the state of the users interactions.
+ *  This is to send an interactive list message, the buttons show up like a dropdown.
+ *  message is a String with the message that will be shown.
+ *  buttons is an array of strings, limited to 20 buttons, according to limitations of this kind o interaction.
+ *  name is a String that will be shown in the dropdown button.
+ *  i is to keep track of the state of the users interactions.
 */
 exports.interactiveListMessage = (from, message, buttons, name, i = 0) => {
 
@@ -218,7 +218,7 @@ exports.interactiveListMessage = (from, message, buttons, name, i = 0) => {
 }
 
 /*
-    This is to send a media message, the image is sent as a URL.
+ *  This is to send a media message, the image is sent as a URL.
 */
 exports.mediaMessage = (from, img) => {
     let body = {
